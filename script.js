40b5e754fd915045e3d9ee9f0945e92d382b046c
carlos505 <aragonaugusto24@gmail.com>
2023-06-17 17:30:23 -0600

    Add more function for the player selection and computer selection

--- script.js
+++ script.js
@@ -10,4 +10,25 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice())
+function playRound(playerSelection, computerSelection) {
+    let playerRound = playerSelection + computerSelection
+    if (playerSelection === rock) {
+        console.log(`Player chose ${rock}, Computer chose ${computerSelection}`)
+    } else if (playerSelection === paper) {
+        console.log(`Player chose ${paper}, Computer chose ${computerSelection}`)
+    } else {
+        console.log(`Player chose ${scissor}, Computer chose ${computerSelection}`)
+    }
+    return playerRound
+}
+
+const playerChoice = prompt('Choose wisely! Rock Paper Scissors')
+const computerChoice = getComputerChoice()
+console.log(playRound(playerChoice, computerChoice))
+
+
+function game(round) {
+    for (let i = 1; i < round.length; i++) {
+
+    }
+}
